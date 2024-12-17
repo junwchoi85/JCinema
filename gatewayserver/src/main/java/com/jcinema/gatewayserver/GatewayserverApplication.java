e15c84ff0422ba47363a527cb06f9ed8960d5a20
Jun Hwan <junwchoi85@gmail.com>
2024-12-17 15:53:31 +1300

    Configuring gatewayserver

--- gatewayserver/src/main/java/com/jcinema/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/jcinema/gatewayserver/GatewayserverApplication.java
@@ -1,13 +1,29 @@
 package com.jcinema.gatewayserver;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator jCinemaRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p.path("/jcinema/users/**")
+						.filters(f -> f.rewritePath("/jcinema/users/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+						.uri("lb://USERS"))
+				.build();
+
 	}
 
 }
