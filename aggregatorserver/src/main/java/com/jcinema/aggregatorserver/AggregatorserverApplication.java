62a4a34c76bd11c217307500eb054c96143d1453
Jun Hwan <junwchoi85@gmail.com>
2025-02-15 15:16:05 +1300

    Introduicng Aggregator Server

---
+++ aggregatorserver/src/main/java/com/jcinema/aggregatorserver/AggregatorserverApplication.java
@@ -0,0 +1,13 @@
+package com.jcinema.aggregatorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatorserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregatorserverApplication.class, args);
+	}
+
+}
---
+++ aggregatorserver/src/test/java/com/jcinema/aggregatorserver/AggregatorserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcinema.aggregatorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatorserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
